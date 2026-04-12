5b4c47abaea995f6c7849ac872111b7fcc51a56e
Mohammed Waleed <mwaleedx1@gmail.com>
2026-04-13 01:41:07 +0200

    Adding Request DTOs

---
+++ CountryBlockerAPI/DTOs/Request DTOs/BlockCountryRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CountryBlockerAPI.DTOs.Request_DTOs
+{
+    public class BlockCountryRequestDto
+    {
+        [Required]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "Country code must be exactly 2 characters.")]
+        public string CountryCode { get; set; } = string.Empty;
+    }
+}
---
+++ CountryBlockerAPI/DTOs/Request DTOs/TemporalBlockRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CountryBlockerAPI.DTOs.Request_DTOs
+{
+    public class TemporalBlockRequestDto
+    {
+        [Required]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "Country code must be exactly 2 characters.")]
+        public string CountryCode { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 1440, ErrorMessage = "Duration must be between 1 and 1440 minutes.")]
+        public int DurationMinutes { get; set; }
+    }
+}
