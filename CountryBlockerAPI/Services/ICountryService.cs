6ce6d7403e44e188b6d1a2672b50915c49c7dddc
Mohammed Waleed <mwaleedx1@gmail.com>
2026-04-13 02:03:05 +0200

    Add CountryService

---
+++ CountryBlockerAPI/Services/ICountryService.cs
@@ -0,0 +1,13 @@
+﻿using CountryBlockerAPI.DTOs.Response_DTOs;
+
+namespace CountryBlockerAPI.Services
+{
+    public interface ICountryService
+    {
+        Task<(bool Success, string Error)> BlockCountryAsync(string countryCode);
+        (bool Success, string Error) UnblockCountry(string countryCode);
+        PagedResponseDto<BlockedCountryResponseDto> GetAllBlocked(int page, int pageSize, string? search);
+
+        Task<(bool Success, string Error, int StatusCode)> AddTemporalBlockAsync(string countryCode, int durationMinutes);
+    }
+}
